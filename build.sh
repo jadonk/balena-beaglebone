3c23e5e73609f82e3f1ecafa652dc5577bfc8f2d
Jason Kridner <jkridner@beagleboard.org>
2020-12-16 13:39:06 -0500

    Add BeagleBone Green Gateway

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+GID=$(getent group docker | cut -d: -f3)
+docker run \
+	-e BUILDER_UID=$(id -u) -e BUILDER_GID=$GID \
+	-v /home/jkridner/workspace/balena-beaglebone:/yocto/resin-board \
+	--privileged \
+	-it resin/yocto-build-env \
+	./inside-docker-build.sh
+
+#	-v /var/run/docker.sock:/var/run/docker.sock \
